001c544b6211e86cdbdd34f709fcdf9fc8efb46a
Axel Niklasson <axel.niklasson@live.com>
2017-12-12 11:24:49 +0100

    Added component for control grid

---
+++ frontend/src/components/ControlGridComponent.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { GridList, GridTile } from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import PowerSvg from 'material-ui/svg-icons/action/power-settings-new';
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  gridList: {
+    display: 'flex',
+    flexWrap: 'nowrap',
+    overflowX: 'auto',
+  },
+  titleStyle: {
+    color: 'rgb(0, 188, 212)',
+  },
+};
+
+export default class ControlsComponent extends React.Component {
+
+  handleClick = (key) => {
+    console.log('Device with key ' + key + ' was toggled.');
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>{this.props.title}</h4>
+        <div style={styles.root}>
+          <GridList style={styles.gridList} cols={2.2}>
+          {this.props.elements.map((el) => (
+            <GridTile
+              key={el.key}
+              onClick={() => {this.handleClick(el.key)}}
+              title={el.title}
+              actionIcon={<IconButton><PowerSvg color="white" /></IconButton>}
+              titleBackground="linear-gradient(to top, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)">
+              <img alt="lamp" src={el.img} />
+            </GridTile>
+          ))}
+          </GridList>
+        </div>
+      </div>
+    );
+  }
+}
+
--- frontend/src/components/DashboardComponent.js
+++ frontend/src/components/DashboardComponent.js
@@ -1,14 +1,53 @@
 import React from 'react';
 import SchemesComponent from './SchemesComponent';
+import ControlGridComponent from './ControlGridComponent';
+import LampImage from './images/lamp.jpg';
+import CoffeMachineImage from './images/coffemachine.png';
 
 export default class DashboardComponent extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lamps: [], 
+      appliances: []
+    };
+  }
+
+  componentDidMount() {
+    this.fetchLamps();
+    this.fetchAppliances();
+  }
+
+  fetchLamps() {
+    let lamps = [
+      { key: 0, img: LampImage, title: 'Sänglampa' },
+      { key: 1, img: LampImage, title: 'Sofflampa' },
+      { key: 2, img: LampImage, title: 'Adventsljusstake' },
+    ];
+
+    this.setState({ lamps });
+  }
+
+  fetchAppliances() {
+    let appliances = [
+      { key: 0, img: CoffeMachineImage, title: 'Kaffebryggare' }
+    ];
+
+    this.setState({ appliances });
+  }
+
+  render = () => {
     return (
       <div>
         <h2>Dashboard</h2>
         
         <h3>Apartment scheme</h3>
         <SchemesComponent />
+
+        <h3>Controls</h3>
+        <ControlGridComponent title="Lamps" elements={this.state.lamps} />
+        <ControlGridComponent title="Appliances" elements={this.state.appliances} />
       </div>
     );
   }
